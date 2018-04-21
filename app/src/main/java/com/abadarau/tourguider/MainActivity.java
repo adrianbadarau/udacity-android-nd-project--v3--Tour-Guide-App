1904f5157e3752c7dc93aa81889644855bb4b6b7
abadarau <abadarau@lenovo.com>
2018-04-21 16:19:33 +0200

    removed debug messages

--- app/src/main/java/com/abadarau/tourguider/MainActivity.java
+++ app/src/main/java/com/abadarau/tourguider/MainActivity.java
@@ -2,12 +2,8 @@ package com.abadarau.tourguider;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
